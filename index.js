c48fc91808c83f148639bc88b68cc6fc0a370c21
Ninkambazi <ninkambazi@gmail.com>
2021-10-23 10:29:44 -0700

    next one

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ if (process.env.NOD_ENV === 'production'){
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, 'client','build')));
 
   // Express serve up index.html file if it doesn't recognize route
   
   app.get('*', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 /*

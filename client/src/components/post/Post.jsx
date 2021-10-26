63726290a472b7ceb53273079b53d13fff61ce35
Ninkambazi <ninkambazi@gmail.com>
2021-10-26 03:14:16 -0700

    commit

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -19,7 +19,7 @@ export default function Post({ post }) {
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`/users?userId=${post.userId}`);
+      const res = await axios.get(`api/users?userId=${post.userId}`);
       setUser(res.data);
     };
     fetchUser();

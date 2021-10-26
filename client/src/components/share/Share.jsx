b80fcb4ab850e500af5668a754c3ef7ce5fb2b6b
Ninkambazi <ninkambazi@gmail.com>
2021-10-26 03:30:33 -0700

    commit

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -30,11 +30,11 @@ export default function Share() {
       newPost.img = fileName;
       console.log(newPost);
       try {
-        await axios.post("api/upload", data);
+        await axios.post("/api/upload", data);
       } catch (err) {}
     }
     try {
-      await axios.post("api/posts", newPost);
+      await axios.post("/api/posts", newPost);
       window.location.reload();
     } catch (err) {}
   };
